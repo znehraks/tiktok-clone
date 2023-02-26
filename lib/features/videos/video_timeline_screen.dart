cce7091b0db18a292385847aba5126bd179c8ca1
znehraks <znehraks@gmail.com>
2023-02-27 00:08:17 +0900

    study: video  study and PageView.builder

---
+++ lib/features/videos/video_timeline_screen.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+
+class VideoTimelineScreen extends StatefulWidget {
+  const VideoTimelineScreen({super.key});
+
+  @override
+  State<VideoTimelineScreen> createState() => _VideoTimelineScreenState();
+}
+
+class _VideoTimelineScreenState extends State<VideoTimelineScreen> {
+  int _itemCount = 4;
+
+  final PageController _pageController = PageController();
+
+  List<Color> colors = [
+    Colors.blue,
+    Colors.red,
+    Colors.yellow,
+    Colors.teal,
+  ];
+
+  void _onPageChanged(int page) {
+    _pageController.animateToPage(
+      page,
+      duration: const Duration(
+        milliseconds: 150,
+      ),
+      curve: Curves.linear,
+    );
+    if (page == _itemCount - 1) {
+      _itemCount += 4;
+      colors.addAll([
+        Colors.blue,
+        Colors.red,
+        Colors.yellow,
+        Colors.teal,
+      ]);
+      setState(() {});
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView.builder(
+      controller: _pageController,
+      scrollDirection: Axis.vertical,
+      onPageChanged: _onPageChanged,
+      itemCount: _itemCount,
+      itemBuilder: (context, index) => Container(
+        color: colors[index],
+        child: Center(
+          child: Text(
+            "Screen $index",
+            style: const TextStyle(
+              fontSize: Sizes.size64,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
