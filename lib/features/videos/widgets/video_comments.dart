c358a9c5c809fc5d96efa40fa491e00da468e674
znehraks <znehraks@gmail.com>
2023-03-03 00:18:53 +0900

    study: start comment (showModalBottomSheet)

---
+++ lib/features/videos/widgets/video_comments.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+
+class VideoComments extends StatefulWidget {
+  const VideoComments({super.key});
+
+  @override
+  State<VideoComments> createState() => _VideoCommentsState();
+}
+
+class _VideoCommentsState extends State<VideoComments> {
+  void _onClosePressed() {
+    Navigator.of(context).pop();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      clipBehavior: Clip.hardEdge,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(Sizes.size14),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.grey.shade50,
+        appBar: AppBar(
+          title: const Text("22796 comments"),
+          centerTitle: true,
+          automaticallyImplyLeading: false,
+          actions: [
+            IconButton(
+              onPressed: _onClosePressed,
+              icon: const FaIcon(FontAwesomeIcons.xmark),
+            ),
+          ],
+        ),
+        body: ListView.builder(
+          itemBuilder: (context, index) => Container(
+            child: const Text("I'm a comment"),
+          ),
+          itemCount: 10,
+        ),
+      ),
+    );
+  }
+}
--- lib/features/videos/widgets/video_post.dart
+++ lib/features/videos/widgets/video_post.dart
@@ -3,6 +3,7 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 import 'package:tiktok_clone/features/videos/widgets/video_button.dart';
+import 'package:tiktok_clone/features/videos/widgets/video_comments.dart';
 import 'package:video_player/video_player.dart';
 import 'package:visibility_detector/visibility_detector.dart';
 
@@ -69,7 +70,9 @@ class _VideoPostState extends State<VideoPost>
   }
 
   void _onVisibilityChanged(VisibilityInfo info) {
-    if (info.visibleFraction == 1 && !_videoPlayerController.value.isPlaying) {
+    if (info.visibleFraction == 1 &&
+        !_videoPlayerController.value.isPlaying &&
+        !_isPaused) {
       _videoPlayerController.play();
     }
   }
@@ -85,6 +88,17 @@ class _VideoPostState extends State<VideoPost>
     setState(() {
       _isPaused = !_isPaused;
     });
+  }
+
+  void _onCommentsTap(BuildContext context) async {
+    if (_videoPlayerController.value.isPlaying) {
+      _onTogglePause();
+    }
+    await showModalBottomSheet(
+      context: context,
+      builder: (context) => const VideoComments(),
+    );
+    _onTogglePause();
   }
 
   @override
@@ -164,8 +178,8 @@ class _VideoPostState extends State<VideoPost>
               bottom: 20,
               right: 10,
               child: Column(
-                children: const [
-                  CircleAvatar(
+                children: [
+                  const CircleAvatar(
                     radius: 25,
                     backgroundColor: Colors.black,
                     foregroundColor: Colors.white,
@@ -175,17 +189,20 @@ class _VideoPostState extends State<VideoPost>
                     child: Text("Jay"),
                   ),
                   Gaps.v24,
-                  VideoButton(
+                  const VideoButton(
                     icon: FontAwesomeIcons.solidHeart,
                     text: "2.9M",
                   ),
                   Gaps.v24,
-                  VideoButton(
-                    icon: FontAwesomeIcons.solidComment,
-                    text: "2.9M",
+                  GestureDetector(
+                    onTap: () => _onCommentsTap(context),
+                    child: const VideoButton(
+                      icon: FontAwesomeIcons.solidComment,
+                      text: "33K",
+                    ),
                   ),
                   Gaps.v24,
-                  VideoButton(
+                  const VideoButton(
                     icon: FontAwesomeIcons.share,
                     text: "Share",
                   ),
